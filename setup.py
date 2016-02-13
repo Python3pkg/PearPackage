4e26be9139cfd39c38d6806c01760eff89f0fec9
Hiroki Umatani <h_umatani@pear.chat>
2016-02-13 17:18:53 +0900

    add project file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "PearMySQL",
+    version = "0.0.2",
+    license = "MIT",
+    packages=['PearMySQL'],
+    description = 'easy mysql lib',
+    author = 'Hiroki Umatani',
+    author_email = 'h_umatani@pear.chat',
+    url = 'https://github.com/HirokiUmatani/PearMySQL',
+    keywords = ['pear', 'mysql', 'query', 'fetch', 'insert', 'update', 'delete'],
+)
