d7199b86a1471922af8257a5bd9dca2af0ed6401
Hiroki Umatani <h_umatani@pear.chat>
2016-06-12 15:55:18 +0900

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="PearPyPac",
-    version="0.3.i",
+    version="0.3.1",
     license="MIT",
     packages=['PearPyPac'],
     description='pear library',
