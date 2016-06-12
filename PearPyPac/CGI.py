e90daed7e04da5d976796b845fb29e8d50014c9e
Hiroki Umatani <h_umatani@pear.chat>
2016-06-12 14:38:52 +0900

    replace cgi's function name

--- PearPyPac/CGI.py
+++ PearPyPac/CGI.py
@@ -7,33 +7,29 @@ import cgi
 class PearCGI(object):
 
 	def __init__(self):
+		self.param = None
 		return
 
-	def isPost(self):
-		if(os.environ["REQUEST_METHOD"] == "POST"):
-			return True
-		else:
-			return False
+	def getMethod(self):
+		methodName = os.environ['REQUEST_METHOD']
+		return methodName
 
-	def isGet(self):
-		if(os.environ["REQUEST_METHOD"] == "GET"):
-			return True
-		else:
-			return False
-
-	def isPut(self):
-		if(os.environ["REQUEST_METHOD"] == "PUT"):
-			return True
-		else:
-			return False
-
-	def isDelete(self):
-		if(os.environ["REQUEST_METHOD"] == "DELETE"):
+	def isMethod(self, methodName):
+		if(self.getMethod() == methodNeme):
 			return True
 		else:
 			return False
 
 	def getParam(self):
-		param = cgi.FieldStorage()
-		return param
+		self.param = cgi.FieldStorage()
+		return self.param
 
+	def getValue(self,valueName):
+		value = self.param.getvalue(valueName)
+		return value
+
+	def isValue(self, value):
+		if not value
+			return False
+		else
+			return True
