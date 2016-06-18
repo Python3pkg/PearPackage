bde66b0b5c2217e95fee09a6b5bde3dc3b72774b
Hiroki Umatani <h_umatani@pear.chat>
2016-06-19 01:09:57 +0900

    check synctax

--- PearPyPac/Util.py
+++ PearPyPac/Util.py
@@ -5,11 +5,11 @@ import configparser
 
 class PearConfig(object):
 
-	def getConfig(self, conf_path):
-		""" set configure """
-		conf = configparser.ConfigParser()
-		conf.read(conf_path)
-		return conf
+    def getConfig(self, conf_path):
+        """ set configure """
+        conf = configparser.ConfigParser()
+        conf.read(conf_path)
+        return conf
 
 if __name__ == '__main__':
-	conf = PearConfig()
+    conf = PearConfig()
